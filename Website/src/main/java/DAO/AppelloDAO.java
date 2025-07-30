41720d6cecd29e37cbf37ba6a94022673d605525
Andrea Pianini <andrea.pianini@mail.polimi.it>
2025-07-30 11:32:47 +0200

    fix

--- Website/src/main/java/DAO/AppelloDAO.java
+++ Website/src/main/java/DAO/AppelloDAO.java
@@ -14,23 +14,11 @@ public class AppelloDAO {
 		this.con=connection;
 	}
 	
-//	public ArrayList<ArrayList<Appello>> GetAppelliByCorsoAndDocente(int docID, int corso) throws SQLException {
-//		
-//		ArrayList<ArrayList<Appello>> appelli = new ArrayList<>();
-//		String query = "SELECT A.corso, A.data FROM Corsi AS C, Docente AS D, Appelli AS A WHERE D.id = ?"
-//				+ "AND C.docente=D.id AND C.id = ? AND C.id=A.corso ORDER BY data DESC";
-//		ResultSet result = null;
-//		PreparedStatement pstatement = null;
-//		pstatement = con.prepareStatement(query);
-//		pstatement.setInt(1, docID);
-//		pstatement.setInt(2, corso);
-//		result = pstatement.executeQuery();
-//	}
 	
-	
-	public Appello GetAppelloByVerbale(int verID) throws SQLException {
-		Appello appello = new Appello();
+	public Appello getAppelloByVerbale(int verID) throws SQLException {
+		
 		String query = "SELECT DISTINCT corso, data FROM Iscrizioni WHERE verbale = ?";
+		Appello appello = new Appello();
 		ResultSet result = null;
 		PreparedStatement pstatement = null;
 		
