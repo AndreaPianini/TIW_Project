12fdef4810697768dd8f0613a2681ecc7ada4bc0
MarcoPdd <puddumarcoo@gmail.com>
2025-08-07 23:35:15 +0200

    fix

--- Website_ThickClient/src/main/webapp/JS/DocenteController.js
+++ Website_ThickClient/src/main/webapp/JS/DocenteController.js
@@ -488,7 +488,7 @@
 			}
 			
 			const VOTI_POSSIBILI = [
-						    "ASSENTE", "RIMANDATO", "RIPROVATO", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "30L"
+						    "", "ASSENTE", "RIMANDATO", "RIPROVATO", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "30L"
 						];
 			
 			class ModificaVoto {
