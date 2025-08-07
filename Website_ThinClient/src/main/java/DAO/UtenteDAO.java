3ca9e5d4f96b4dd0a1a12e763ab638081a43ece6
Andrea <andrea.pianini@mail.polimi.it>
2025-08-07 23:27:21 +0200

    Update UtenteDAO.java

--- Website_ThinClient/src/main/java/DAO/UtenteDAO.java
+++ Website_ThinClient/src/main/java/DAO/UtenteDAO.java
@@ -18,54 +18,6 @@ public class UtenteDAO {
 	}
 	
 	
-//	public Utente checkCredenziali(int ID, String pwd) throws SQLException{
-//		
-//	    String query1 = "SELECT * FROM Utenti WHERE id = ? AND password = ?";
-//        PreparedStatement pstmt = connection.prepareStatement(query1);
-//        pstmt.setInt(1, ID);
-//        pstmt.setString(2, pwd);
-//        ResultSet resultSet = pstmt.executeQuery();
-//        // Check if the result set has any rows
-//        if (resultSet.next()) {
-//        	String query2 = "SELECT * FROM Studenti WHERE id = '" + ID +"'";
-//            Statement stmt2 = connection.createStatement();
-//            ResultSet resultSet2 = stmt2.executeQuery(query2);
-//            //The user is a student
-//            if (resultSet2.next()) {
-//				Studente studente = new Studente();
-//				studente.setID(resultSet.getInt("id"));
-//				studente.setNome(resultSet.getString("nome"));
-//				studente.setCognome(resultSet.getString("cognome"));
-//				studente.setEmail(resultSet.getString("email"));
-//				studente.setMatricola(resultSet2.getString("matricola"));
-//				studente.setCorsoLaurea(resultSet2.getString("corso_laurea"));
-//				return studente;
-//			}
-//            //The user should be a teacher
-//            else {
-//            	String query3 = "SELECT * FROM Docenti WHERE id = '" + ID +"'";
-//                Statement stmt3 = connection.createStatement();
-//                ResultSet resultSet3 = stmt3.executeQuery(query3);
-//				if (resultSet3.next()) {
-//					Docente docente = new Docente();
-//					docente.setID(resultSet.getInt("id"));
-//					docente.setNome(resultSet.getString("nome"));
-//					docente.setCognome(resultSet.getString("cognome"));
-//					docente.setEmail(resultSet.getString("email"));
-//					return docente;
-//				}
-//				// The user is neither a student nor a teacher - error in the database
-//				else
-//					return null; // No matching user found
-//            }
-//		}
-//        //No matching user found (the result set is empty)
-//		else {
-//			return null;
-//		}
-//        
-//	}
-	
 	public Utente checkCredenziali(int ID, String pwd) throws SQLException {
 	    String queryUtente = "SELECT * FROM Utenti WHERE id = ? AND password = ?";
 	    try (PreparedStatement pstmt = connection.prepareStatement(queryUtente)) {
